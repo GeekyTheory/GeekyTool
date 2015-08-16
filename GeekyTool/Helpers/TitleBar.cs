c894e52619a745f0532d4b551c0524f484502636
Dachi Gogotchuri <dachi@geekytheory.com>
2015-08-16 20:07:10 +0200

    TitleBar helper implemented
    https://marcominerva.wordpress.com/2015/05/19/easily-manage-the-title-bar-in-windows-10-apps/

---
+++ GeekyTool/Helpers/TitleBar.cs
@@ -0,0 +1,104 @@
+﻿using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+
+namespace GeekyTool.Helpers
+{
+    /// <summary>
+    /// As seen at: https://marcominerva.wordpress.com/2015/05/19/easily-manage-the-title-bar-in-windows-10-apps/
+    /// </summary>
+    public static class TitleBar
+    {
+        public static readonly DependencyProperty ForegroundColorProperty =
+            DependencyProperty.RegisterAttached("ForegroundColor", typeof(Color),
+            typeof(TitleBar),
+            new PropertyMetadata(null, OnForegroundColorPropertyChanged));
+
+        public static Color GetForegroundColor(DependencyObject d)
+        {
+            return (Color)d.GetValue(ForegroundColorProperty);
+        }
+
+        public static void SetForegroundColor(DependencyObject d, Color value)
+        {
+            d.SetValue(ForegroundColorProperty, value);
+        }
+
+        private static void OnForegroundColorPropertyChanged(DependencyObject d,
+            DependencyPropertyChangedEventArgs e)
+        {
+            var color = (Color)e.NewValue;
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ForegroundColor = color;
+        }
+
+        public static readonly DependencyProperty BackgroundColorProperty =
+            DependencyProperty.RegisterAttached("BackgroundColor", typeof(Color),
+            typeof(TitleBar),
+            new PropertyMetadata(null, OnBackgroundColorPropertyChanged));
+
+        public static Color GetBackgroundColor(DependencyObject d)
+        {
+            return (Color)d.GetValue(BackgroundColorProperty);
+        }
+
+        public static void SetBackgroundColor(DependencyObject d, Color value)
+        {
+            d.SetValue(BackgroundColorProperty, value);
+        }
+
+        private static void OnBackgroundColorPropertyChanged(DependencyObject d,
+            DependencyPropertyChangedEventArgs e)
+        {
+            var color = (Color)e.NewValue;
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.BackgroundColor = color;
+        }
+
+        public static readonly DependencyProperty ButtonForegroundColorProperty =
+            DependencyProperty.RegisterAttached("ButtonForegroundColor", typeof(Color),
+            typeof(TitleBar),
+            new PropertyMetadata(null, OnButtonForegroundColorPropertyChanged));
+
+        public static Color GetButtonForegroundColor(DependencyObject d)
+        {
+            return (Color)d.GetValue(ButtonForegroundColorProperty);
+        }
+
+        public static void SetButtonForegroundColor(DependencyObject d, Color value)
+        {
+            d.SetValue(ButtonForegroundColorProperty, value);
+        }
+
+        private static void OnButtonForegroundColorPropertyChanged(DependencyObject d,
+            DependencyPropertyChangedEventArgs e)
+        {
+            var color = (Color)e.NewValue;
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ButtonForegroundColor = color;
+        }
+
+        public static readonly DependencyProperty ButtonBackgroundColorProperty =
+            DependencyProperty.RegisterAttached("ButtonBackgroundColor", typeof(Color),
+            typeof(TitleBar),
+            new PropertyMetadata(null, OnButtonBackgroundColorPropertyChanged));
+
+        public static Color GetButtonBackgroundColor(DependencyObject d)
+        {
+            return (Color)d.GetValue(ButtonBackgroundColorProperty);
+        }
+
+        public static void SetButtonBackgroundColor(DependencyObject d, Color value)
+        {
+            d.SetValue(ButtonBackgroundColorProperty, value);
+        }
+
+        private static void OnButtonBackgroundColorPropertyChanged(DependencyObject d,
+            DependencyPropertyChangedEventArgs e)
+        {
+            var color = (Color)e.NewValue;
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ButtonBackgroundColor = color;
+        }
+    }
+}
