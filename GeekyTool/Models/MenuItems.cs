f8d22b7b2a3157d184d3496851f9b3d2389f4c6a
dachibox <dachi@geekytheory.com>
2015-08-21 23:40:39 +0200

    - Added MenuItems as Singelton.

---
+++ GeekyTool/Models/MenuItems.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekyTool.Models
+{
+    public class MenuItems
+    {
+        public static MenuItems instance = null;
+
+        private MenuItems() { }
+
+        public static MenuItems Instance()
+        {
+            if (instance == null)
+                instance = new MenuItems();
+            return instance;
+        }
+
+        public ObservableCollection<MenuItem> Items { get; set; }
+    }
+}
