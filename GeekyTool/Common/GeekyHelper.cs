7a8a5663bcef7699b9046ffddb254f05125e425b
dachibox <dachi@geekytheory.com>
2015-08-23 12:07:46 +0200

    - ExtractAllImagesFromHtml imlemented in GeekyHelper

--- GeekyTool/Common/GeekyHelper.cs
+++ GeekyTool/Common/GeekyHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using Windows.UI;
 using Windows.UI.Xaml.Media;
@@ -11,6 +12,20 @@ namespace GeekyTool
         {
             string matchString = Regex.Match(content, "<img.+?src=[\"'](.+?)[\"'].*?>", RegexOptions.IgnoreCase).Groups[1].Value;
             return matchString;
+        }
+
+        private List<string> ExtractAllImagesFromHtml(string content)
+        {
+            List<string> images = new List<string>();
+            
+            MatchCollection matches = Regex.Matches(content, "<img.+?src=[\"'](.+?)[\"'].*?>", RegexOptions.IgnoreCase);
+
+            for (int i = 0, l = matches.Count; i < l; i++)
+            {
+                images.Add(matches[i].Value);
+            }
+
+            return images;
         }
 
         public static SolidColorBrush GetBrushColorFromHexa(string hexaColor)
