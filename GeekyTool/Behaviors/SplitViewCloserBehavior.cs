aafd650560029d76c98087b75d5988bfbd280fc0
Gabriel Ferreiro <gbril9119@gmail.com>
2015-08-30 20:10:10 +0200

    Added SplitViewOpener/Closer Behaviors

---
+++ GeekyTool/Behaviors/SplitViewCloserBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Microsoft.Xaml.Interactivity;
+
+namespace GeekyTool.Behaviors
+{
+    public class SplitViewCloserBehavior : DependencyObject, IBehavior
+    {
+        public SplitView Splitter
+        {
+            get { return (SplitView)GetValue(SplitterProperty); }
+            set { SetValue(SplitterProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Splitter.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SplitterProperty =
+            DependencyProperty.Register("Splitter", typeof(SplitView), typeof(SplitViewCloserBehavior), new PropertyMetadata(null));
+        
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            var fe = associatedObject as FrameworkElement;
+
+            if (fe == null) return;
+
+            fe.ManipulationMode = ManipulationModes.TranslateX;
+            fe.ManipulationCompleted += AssociatedObjectOnManipulationCompleted;
+        }
+
+        private void AssociatedObjectOnManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs manipulationCompletedRoutedEventArgs)
+        {
+            if (manipulationCompletedRoutedEventArgs.Cumulative.Translation.X < -50)
+                Splitter.IsPaneOpen = false;
+        }
+
+        public void Detach()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DependencyObject AssociatedObject { get; }
+    }
+}
